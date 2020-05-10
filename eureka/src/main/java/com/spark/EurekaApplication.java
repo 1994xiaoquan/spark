e1b66d9fc5cb03b851b3953aa6b93aae923dd566
1994xiaoquan <995680996@qq.com>
2020-05-10 15:56:33 +0800

    1.增加了eureka子模块作为注册服务端 2.maven项目改造为maven父子项目

--- eureka/src/main/java/com/spark/EurekaApplication.java
+++ eureka/src/main/java/com/spark/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.spark.spark;
+package com.spark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SparkApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SparkApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
